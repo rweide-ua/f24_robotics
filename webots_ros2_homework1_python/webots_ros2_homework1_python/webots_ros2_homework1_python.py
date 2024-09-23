85250ed5f39fa8e6b899c696084408d7942ba7a3
rweide <rweide@crimson.ua.edu>
2024-09-23 17:01:45 -0500

    Start wall follow algo

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -62,7 +62,7 @@ class RandomWalk(Node):
             elif math.isnan(reading):
                 self.scan_cleaned.append(0.0)
             else:
-            	self.scan_cleaned.append(reading)
+                self.scan_cleaned.append(reading)
 
 
 
@@ -87,9 +87,9 @@ class RandomWalk(Node):
         
     def timer_callback(self):
         if (len(self.scan_cleaned)==0):
-    	    self.turtlebot_moving = False
-    	    return
-    	    
+            self.turtlebot_moving = False
+            return
+            
         #left_lidar_samples = self.scan_cleaned[LEFT_SIDE_INDEX:LEFT_FRONT_INDEX]
         #right_lidar_samples = self.scan_cleaned[RIGHT_FRONT_INDEX:RIGHT_SIDE_INDEX]
         #front_lidar_samples = self.scan_cleaned[LEFT_FRONT_INDEX:RIGHT_FRONT_INDEX]
@@ -135,17 +135,135 @@ class RandomWalk(Node):
         # Display the message on the console
         self.get_logger().info('Publishing: "%s"' % self.cmd)
  
+class WallFollow(Node):
 
+    def __init__(self):
+        # Initialize the publisher
+        super().__init__('wall_follow_node')
+        self.scan_cleaned = []
+        self.stall = False
+        self.turtlebot_moving = False
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.subscriber1 = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.listener_callback1,
+            QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.subscriber2 = self.create_subscription(
+            Odometry,
+            '/odom',
+            self.listener_callback2,
+            QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.laser_forward = 0
+        self.odom_data = 0
+        timer_period = 0.5
+        self.pose_saved=''
+        self.cmd = Twist()
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    # Reads scan and cleans it
+    def listener_callback1(self, msg1):
+        #self.get_logger().info('scan: "%s"' % msg1.ranges)
+        scan = msg1.ranges
+        self.scan_cleaned = []
+       
+        #self.get_logger().info('scan: "%s"' % scan)
+        # Assume 360 range measurements
+        for reading in scan:
+            if reading == float('Inf'):
+                self.scan_cleaned.append(3.5)
+            elif math.isnan(reading):
+                self.scan_cleaned.append(0.0)
+            else:
+                self.scan_cleaned.append(reading)
+
+    
+    # Log position
+    def listener_callback2(self, msg2):
+        position = msg2.pose.pose.position
+        orientation = msg2.pose.pose.orientation
+        (posx, posy, posz) = (position.x, position.y, position.z)
+        (qx, qy, qz, qw) = (orientation.x, orientation.y, orientation.z, orientation.w)
+        self.get_logger().info('self position: {},{},{}'.format(posx,posy,posz));
+        # similarly for twist message if you need
+        self.pose_saved=position
+        
+        #Example of how to identify a stall..need better tuned position deltas; wheels spin and example fast
+        #diffX = math.fabs(self.pose_saved.x- position.x)
+        #diffY = math.fabs(self.pose_saved.y - position.y)
+        #if (diffX < 0.0001 and diffY < 0.0001):
+           #self.stall = True
+        #else:
+           #self.stall = False
+           
+        return None
+
+
+    # After certain period of time, move in random direction?    
+    def timer_callback(self):
+        if (len(self.scan_cleaned)==0):
+            self.turtlebot_moving = False
+            return
+            
+        #left_lidar_samples = self.scan_cleaned[LEFT_SIDE_INDEX:LEFT_FRONT_INDEX]
+        #right_lidar_samples = self.scan_cleaned[RIGHT_FRONT_INDEX:RIGHT_SIDE_INDEX]
+        #front_lidar_samples = self.scan_cleaned[LEFT_FRONT_INDEX:RIGHT_FRONT_INDEX]
+        
+        left_lidar_min = min(self.scan_cleaned[LEFT_SIDE_INDEX:LEFT_FRONT_INDEX])
+        right_lidar_min = min(self.scan_cleaned[RIGHT_FRONT_INDEX:RIGHT_SIDE_INDEX])
+        front_lidar_min = min(self.scan_cleaned[LEFT_FRONT_INDEX:RIGHT_FRONT_INDEX])
+
+        #self.get_logger().info('left scan slice: "%s"'%  min(left_lidar_samples))
+        #self.get_logger().info('front scan slice: "%s"'%  min(front_lidar_samples))
+        #self.get_logger().info('right scan slice: "%s"'%  min(right_lidar_samples))
+
+        if front_lidar_min < SAFE_STOP_DISTANCE:
+            if self.turtlebot_moving == True:
+                self.cmd.linear.x = 0.0 
+                self.cmd.angular.z = 0.0 
+                self.publisher_.publish(self.cmd)
+                self.turtlebot_moving = False
+                self.get_logger().info('Stopping')
+                return
+        elif front_lidar_min < LIDAR_AVOID_DISTANCE:
+                self.cmd.linear.x = 0.07 
+                if (right_lidar_min > left_lidar_min):
+                   self.cmd.angular.z = -0.3
+                else:
+                   self.cmd.angular.z = 0.3
+                self.publisher_.publish(self.cmd)
+                self.get_logger().info('Turning')
+                self.turtlebot_moving = True
+        else:
+            self.cmd.linear.x = 0.3
+            self.cmd.linear.z = 0.0
+            self.publisher_.publish(self.cmd)
+            self.turtlebot_moving = True
+            
+
+        self.get_logger().info('Distance of the obstacle : %f' % front_lidar_min)
+        self.get_logger().info('I receive: "%s"' %
+                               str(self.odom_data))
+        if self.stall == True:
+           self.get_logger().info('Stall reported')
+        
+        # Display the message on the console
+        self.get_logger().info('Publishing: "%s"' % self.cmd)    
 
 def main(args=None):
     # initialize the ROS communication
     rclpy.init(args=args)
     # declare the node constructor
-    random_walk_node = RandomWalk()
+    # random_walk_node = RandomWalk()
+
+    wall_follow_node = WallFollow()
     # pause the program execution, waits for a request to kill the node (ctrl+c)
-    rclpy.spin(random_walk_node)
+    # rclpy.spin(random_walk_node)
+    rclpy.spin(wall_follow_node)
+
     # Explicity destroy the node
-    random_walk_node.destroy_node()
+    # random_walk_node.destroy_node()
+    wall_follow_node.destroy_node()
     # shutdown the ROS communication
     rclpy.shutdown()
 
